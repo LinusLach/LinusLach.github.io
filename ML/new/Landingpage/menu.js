8495d80239f479adbb4637b5cc5a529fe405e41a
Linus <Linus.lach@uni-a.de>
2024-09-12 22:28:59 +0200

    added presentation for statistische woche

--- ML/new/Landingpage/menu.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.overlay-link').forEach(link => {
-    link.addEventListener('click', function() {
-        // Uncheck the checkbox to close the overlay
-        document.getElementById('overlay-input').checked = false;
-    });
-});
